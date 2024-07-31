42be3927fb1dc2de781942d8575ab19f05186af1
PRoy <sam469roger@gmail.com>
2024-07-31 06:01:32 +0530

    feat: implemented database seeding

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
+const axios = require('axios').default;
+const Transaction = require('../models/transaction');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB database connected...');
-  } catch (err) {
-    console.error(err.message);
+
+    await seedDatabase();
+  } catch (error) {
+    console.error(error.message);
     process.exit(1);
+  }
+};
+
+const seedDatabase = async () => {
+  try {
+    await Transaction.deleteMany({});
+    console.log('Database cleared...');
+
+    const { data } = await axios.get(
+      'https://s3.amazonaws.com/roxiler.com/product_transaction.json'
+    );
+
+    await Transaction.insertMany(data);
+    console.log('Database seeded...');
+  } catch (error) {
+    console.log('Error seeding database:', error);
   }
 };
 
